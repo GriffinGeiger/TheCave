636ca21575f550dcd7e199b7a482c7b26757d191
GriffinGeiger <griffin2420@yahoo.com>
2018-11-11 15:52:56 -0500

    made a second scene to reimplement the game in 2d. Cave scenery and parralax effect built

---
+++ Assets/Scripts/PlayerMovement2D.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2D : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    public Animator anime;
+
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    void Awake()
+    {
+        controller.OnLandEvent.AddListener(OnLanding);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (anime != null)
+            anime.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Jumping");
+            jump = true;
+            if(anime != null)
+                anime.SetBool("Jumping", true);
+            //controller.timeOfJump = Time.time;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+    public void OnLanding()
+    {
+        anime.SetBool("Jumping", false);
+    }
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
