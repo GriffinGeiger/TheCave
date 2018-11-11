ca968aecd3106d29d456073cc3b4002d9d783c97
GriffinGeiger <griffin2420@yahoo.com>
2018-11-10 23:36:24 -0500

    initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public CharacterController controller;
+    public Animator anime;
+
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+        anime.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if(Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Jumping");
+            jump = true;
+            anime.SetBool("IsJumping", true);
+            controller.timeOfJump = Time.time;
+        }
+        
+        if(Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if(Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+	}
+    public void OnLanding()
+    {
+        anime.SetBool("IsJumping", false);
+    }
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
